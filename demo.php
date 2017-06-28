2e69e1b9b39c02cc75126bae380653cfd8e9af59
王辉 <874075731@qq.com>
2017-06-28 22:50:18 +0800

    铭感词库查询过滤1.0

--- demo.php
+++ demo.php
@@ -5,8 +5,30 @@
  * Date: 17/6/28
  * Time: 下午5:54
  */
+header('Content-type: text/html; charset=utf-8');
+date_default_timezone_set('PRC');
 
+require_once 'class/LSensitiveWordFilter.php';
+
+$sTime = microtime(true);
+
+//获取铭感词库
 $wordPool = file_get_contents('keyWord.txt');
-
+$wordData = explode(',', $wordPool);
 //构建敏感词hashMap
 $sensitiveWordMap = LSensitiveWordFilter::init()->setHashMap($wordData);
+$content = $_POST['content'];
+$filterContent = LSensitiveWordFilter::init()->replaceSensitiveWord($sensitiveWordMap, $content, '***');
+
+$eTime = microtime(true);
+
+//结果
+echo '<hr/>';
+echo '<hr/>';
+echo '<hr/>';
+echo '检测后结果：' . $filterContent . '<br/>';
+echo '运行时间：' . ($eTime - $sTime) * 1000 . '(ms)';
+echo '<hr/>';
+echo '<hr/>';
+echo '<hr/>';
+
