2d026c1efd9e9af5d26c70f46dddca1e41644c5b
王辉 <874075731@qq.com>
2017-06-29 20:22:12 +0800

    增加词库

--- class/LHashMap.php
+++ class/LHashMap.php
@@ -5,7 +5,7 @@
  * User: wanghui
  * Date: 17/3/9
  * Time: 上午9:10
- */
+ **/
 class LHashMap
 {
 
@@ -19,11 +19,11 @@ class LHashMap
         $this->H_table = array();
     }
 
-    /*
-     *向HashMap中添加一个键值对
-     *@param $key 插入的键
-     *@param $value 插入的值
-    */
+    /**
+     * 向HashMap中添加一个键值对
+     * @param $key 插入的键
+     * @param $value 插入的值
+     **/
     public function put($key, $value)
     {
         if (!array_key_exists($key, $this->H_table)) {
@@ -36,10 +36,10 @@ class LHashMap
         }
     }
 
-    /*
-    * 根据key获取对应的value
-    * @param $key
-    */
+    /**
+     * 根据key获取对应的value
+     * @param $key
+     **/
     public function get($key)
     {
         if (array_key_exists($key, $this->H_table))
@@ -48,10 +48,10 @@ class LHashMap
             return null;
     }
 
-    /*
+    /**
      * 删除指定key的键值对
      * @param $key 要移除键值对的key
-     */
+     **/
     public function remove($key)
     {
         $temp_table = array();
@@ -73,7 +73,7 @@ class LHashMap
     /**
      * 获取HashMap的所有键值
      * @return 返回HashMap中key的集合,以数组形式返回
-     */
+     **/
     public function keys()
     {
         return array_keys($this->H_table);
@@ -90,7 +90,7 @@ class LHashMap
     /**
      * 将一个HashMap的值全部put到当前HashMap中
      * @param $map
-     */
+     **/
     public function putAll($map)
     {
         if (!$map->isEmpty() && $map->size() > 0) {
@@ -103,17 +103,17 @@ class LHashMap
 
     /**
      * 移除HashMap中所有元素
-     */
+     **/
     public function removeAll()
     {
         $this->H_table = null;
         $this->H_table = array();
     }
 
-    /*
-     *HashMap中是否包含指定的值
-     *@param $value
-    */
+    /**
+     * HashMap中是否包含指定的值
+     * @param $value
+     **/
     public function containsValue($value)
     {
         while ($curValue = current($this->H_table)) {
@@ -125,10 +125,10 @@ class LHashMap
         return false;
     }
 
-    /*
-     *HashMap中是否包含指定的键key
-     *@param $key
-    */
+    /**
+     * HashMap中是否包含指定的键key
+     * @param $key
+     **/
     public function containsKey($key)
     {
         if (array_key_exists($key, $this->H_table)) {
@@ -138,17 +138,17 @@ class LHashMap
         }
     }
 
-    /*
-     *获取HashMap中元素个数
-     */
+    /**
+     * 获取HashMap中元素个数
+     **/
     public function size()
     {
         return count($this->H_table);
     }
 
-    /*
-    *判断HashMap是否为空
-    */
+    /**
+     * 判断HashMap是否为空
+     **/
     public function isEmpty()
     {
         return (count($this->H_table) == 0);
